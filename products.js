7af5b619fd69c58883286cfec4debf2d77b03b17
Pheeraphat Jaiboon <69642654+Pheerapht@users.noreply.github.com>
2022-04-27 11:41:43 +0700

    Add files via upload

---
+++ products.js
@@ -0,0 +1,39 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+  123: {
+    name : "เสื้อนักศึกษาชาย",
+    desc : ".............................",
+    img : "shirt_men.png",
+    price : 230
+  },
+  124: {
+    name : "เสื้อนักศึกษาชาย",
+    desc : ".............................",
+    img : "shirt_women.png",
+    price : 250
+  },
+  125: {
+    name : "กระโปรง",
+    desc : ".............................",
+    img : "women.png",
+    price : 275
+  },
+  126: {
+    name : "กระโปรง",
+    desc : ".............................",
+    img : "women.png",
+    price : 275
+  },
+  127: {
+    name : "เสื้อสูทดำ",
+    desc : ".............................",
+    img : "men_suit_black.png",
+    price : 675
+  },
+  128: {
+    name : "เสื้อKMUTNB",
+    desc : ".............................",
+    img : "run_shirt.jpg",
+    price : 675
+  }
+};
